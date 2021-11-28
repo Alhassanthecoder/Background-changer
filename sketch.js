3ac5b5d47b2cce76466f1ef5c41f164255c6d1a6
Alhassanthecoder <95006076+Alhassanthecoder@users.noreply.github.com>
2021-11-28 18:09:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(1920,1080);
+
+}
+
+function draw() 
+{
+
+  background("white");{
+if (keyIsDown(RIGHT_ARROW)) 
+ background ("green");
+}
+
+  drawSprites()
+}
+
+
+
+
